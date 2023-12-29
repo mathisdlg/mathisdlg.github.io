191ce6227ca20a383b2322a9a9b6c863d0e64013
Mathis <mathis.delage@etu.uca.fr>
2023-12-30 00:56:26 +0100

    Test FrontController

--- PHP/src/Controller/FrontController.php
+++ PHP/src/Controller/FrontController.php
@@ -4,6 +4,20 @@ namespace Controller;
 
 class FrontController{
     public function __construct(){
+        global $twig;
+        global $basePath;
         
+        try{
+            // $router = new \AltoRouter();     Alto router for later
+            // $router->setBasePath($basePath);
+            
+            $dView['basePath'] = $basePath;
+            $dView['title'] = "Welcome";
+            $dView['error'] = false;
+            
+            echo $twig->render("WebSite/Welcome/Welcome.html", ['dView' => $dView]);
+        }catch(\Exception $e){
+            echo $e->getMessage();
+        }
     }
 }
