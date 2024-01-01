bdf07ebe342b0a25c50991980153bb8a7848c77f
Mathis <mathis.delage@etu.uca.fr>
2024-01-01 23:41:13 +0100

    Do the logic of the front controller

--- PHP/src/Controller/FrontController.php
+++ PHP/src/Controller/FrontController.php
@@ -1,23 +1,73 @@
 <?php
 
-namespace Controller;
-
-class FrontController{
-    public function __construct(){
-        global $twig;
-        global $basePath;
+namespace Controller{
+    
+    class FrontController{
+        public function __construct(){
+            global $twig;
+            global $basePath;
+            
+            try{
+                $dView['basePath'] = $basePath;
+                
+                $router = new \AltoRouter();
+                $router->setBasePath($basePath);
+                
+                $router->map('GET|POST', '[/]?', 'CtrlVisitor');
+                $router->map('GET|POST', '/[a:action][/]?', 'CtrlVisitor');
+                
+                
+                $match = $router->match();
+                if (!$match){
+                    $this->error($dView, 404, "Not Found", "The requested URL was not found on this server.");
+                }
+                else{
+                    $dView['match'] = $match;
+                    $dView['title'] = "Welcome";
+                    
+                    switch ($match['target']){
+                        case 'CtrlVisitor':
+                            $this->callController("CtrlVisitor", $dView);
+                            break;
+                        default:
+                            $this->error($dView, 404, "Not Found", "The requested URL was not found on this server.");
+                            break;
+                    }
+                }
+            }catch(\Exception $e){
+                $this->error($dView, 500, "Internal server error", $e->getMessage());
+            }
+        }
         
-        try{
-            // $router = new \AltoRouter();     Alto router for later
-            // $router->setBasePath($basePath);
+        private function callController(string $ctrl, array $dView){
+            global $twig;
             
-            $dView['basePath'] = $basePath;
-            $dView['title'] = "Welcome";
-            $dView['error'] = false;
+            if(isset($dView['match']['params']['action'])){
+                $action = $dView['match']['params']['action'];
+            }
+            else{
+                $action = "Welcome";
+            }
             
-            echo $twig->render("WebSite/Welcome/Welcome.html", ['dView' => $dView]);
-        }catch(\Exception $e){
-            echo $e->getMessage();
+            $controller = "Controller\\" . $ctrl;
+            $controller = new $controller();
+            
+            if (is_callable(array($controller, $action))){
+                call_user_func_array(array($controller, $action), array($dView));
+            }
+            else{
+                $this->error($dView, 404, "Not Found", "The requested URL was not found on this server.");
+            }
+        }
+        
+        private static function error(array $dView, int $code, string $message, string $desc){
+            global $twig;
+            
+            http_response_code($code);
+            $dView['title'] = $code . " " . $message;
+            $dView['desc'] = $desc;
+            
+            echo $twig->render("Common/Error.html", ['dView' => $dView]);
         }
     }
 }
