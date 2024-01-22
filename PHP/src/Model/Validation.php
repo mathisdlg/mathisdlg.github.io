5de246e54f5fde5044fa6b26b75c1c4f166b0e93
Mathis <mathis.delage@etu.uca.fr>
2024-01-22 16:01:47 +0100

    Creation of the validation function for page

--- PHP/src/Model/Validation.php
+++ PHP/src/Model/Validation.php
@@ -1,15 +1,25 @@
 <?php
 
-    class Validation
+namespace Model;
+
+class Validation
+{
+    
+    private static function filterValidate(&$input, int $validator, array $regex=null) : bool
     {
-        
-        private static function filterValidate(&$input, int $sanitizer, int $validator) : bool
-        {
-            $sanitized = filter_var($input, $sanitizer);
-            if (filter_var($sanitized, $validator) !== false) {
-                $input = $sanitized;
-                return true;
-            }
-            return false;
+        if ($regex != null){
+            return filter_var($input, $validator, $regex);
         }
+        return filter_var($input, $validator);
     }
+    
+    private static function filterRegex($input, string $regex) : bool
+    {
+        return preg_match($regex, $input);
+    }
+    
+    public static function isPage($input) : bool
+    {
+        return self::filterRegex($input, "/^[A-Z][a-zA-Z]+$/");
+    }
+}
